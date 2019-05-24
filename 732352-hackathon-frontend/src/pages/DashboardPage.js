3f7e3cee5eeec197c30e39d852d725d6b8c6afba
Boopathi Sakthivel <boopathi_sakthivel@intuit.com>
2019-05-24 17:39:01 +0530

    integrated backend services

--- 732352-hackathon-frontend/src/pages/DashboardPage.js
+++ 732352-hackathon-frontend/src/pages/DashboardPage.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import ThreeDRotationIcon from '@material-ui/icons/ThreeDRotation';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-
-const classes = {
-  card: {
-    maxWidth: 200,
-  },
-};
 
 class DashboardPage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   onCardClick = (title) => {
     window.location.href = "/#" + title;
   }
